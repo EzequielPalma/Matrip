2b61e3c72bad96761f03c842a7a63e2f0826a840
EzequielPalma <ezequielpalmat@gmail.com>
2024-01-15 12:33:00 +0100

    cat a a a

---
+++ src/app/components/Descripcion.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { motion } from 'framer-motion';
+
+
+export default function Descripcion() {
+  return (
+          <motion.div
+          initial={{ opacity: 0, scale: 0.1 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 3 }}
+          >   
+            <div className="bg-gray-800 bg-opacity-75 p-8 rounded-md shadow-lg mb-20 m-2 rounded-md">
+                <h1 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+                Madrid, donde cada pisada es una melodía que resuena en tu corazón. Deja que esta ciudad te envuelva con su encanto, susurros de historia y la calidez de su gente. Cada calle, cada plaza, es una sinfonía de experiencias que se fusionan para crear recuerdos imborrables. Ven y descubre cómo Madrid te cautiva con cada paso, dejando huellas de amor en tu corazón.
+                </h1>
+            </div>
+          </motion.div>
+
+
+
+  )
+}
--- src/app/components/FrontPage.js
+++ src/app/components/FrontPage.js
@@ -1,6 +1,6 @@
 'use client'
-import React, { useState } from 'react';
 import Navbar from './Navbar';
+import Descripcion from './Descripcion';
 
 const FrontPage = () => {
 
@@ -10,11 +10,7 @@ const FrontPage = () => {
 
       
       <header id="up" className="bg-center bg-fixed bg-no-repeat bg-center bg-cover h-screen relative">
-        <div className="bg-gray-800 bg-opacity-75 p-8 rounded-md shadow-lg mb-20 m-2 rounded-md">
-          <h1 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
-            Madrid, donde cada pisada es una melodía que resuena en tu corazón. Deja que esta ciudad te envuelva con su encanto, susurros de historia y la calidez de su gente. Cada calle, cada plaza, es una sinfonía de experiencias que se fusionan para crear recuerdos imborrables. Ven y descubre cómo Madrid te cautiva con cada paso, dejando huellas de amor en tu corazón.
-          </h1>
-        </div>
+        <Descripcion/>
       </header>
 
     </div>
