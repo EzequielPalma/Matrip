56a5ff6f474bfc438e625594ccc74af21384081a
EzequielPalma <ezequielpalmat@gmail.com>
2024-01-15 14:58:13 +0100

    i9giug

--- src/app/components/Descripcion.js
+++ src/app/components/Descripcion.js
@@ -10,7 +10,7 @@ export default function Descripcion() {
     >
       <div className="bg-gray-800 shadow-md bg-opacity-75 p-8 rounded-md shadow-lg mb-20 mx-auto my-2 md:w-2/3 lg:w-1/2 xl:w-1/3">
         <h1 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
-          "Madrid, donde cada pisada es una melodía que resuena en tu corazón. Deja que esta ciudad te envuelva con su encanto, susurros de historia y la calidez de su gente. Cada calle, cada plaza, es una sinfonía de experiencias que se fusionan para crear recuerdos imborrables. Ven y descubre cómo Madrid te cautiva con cada paso, dejando huellas de amor en tu corazón."
+          Madrid, donde cada pisada es una melodía que resuena en tu corazón. Deja que esta ciudad te envuelva con su encanto, susurros de historia y la calidez de su gente. Cada calle, cada plaza, es una sinfonía de experiencias que se fusionan para crear recuerdos imborrables. Ven y descubre cómo Madrid te cautiva con cada paso, dejando huellas de amor en tu corazón.
         </h1>
       </div>
 
