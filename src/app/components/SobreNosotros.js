7fd4fbf2c51ece1a0ee17e9e59c80d725f1f7c5f
EzequielPalma <ezequielpalmat@gmail.com>
2024-01-18 08:59:53 +0100

    Se quitado el modo dark de los botones de Sobre nosotros.

--- src/app/components/SobreNosotros.js
+++ src/app/components/SobreNosotros.js
@@ -14,10 +14,10 @@ export default function SobreNosotros() {
           <p className="text-white text-sm  sm:text-lg mt-4 text-left">En MATRIP, sumérgete en una experiencia única donde cada rincón de Madrid cobra vida con magia. Somos tu guía para un viaje lleno de descubrimientos, sabores y secretos, transportándote a un Madrid encantador. Descubre tesoros ocultos, explora calles empedradas con historias olvidadas, encuentra puertas secretas hacia dimensiones insospechadas y deja que tu curiosidad se despierte en los encantos ocultos de la ciudad.</p>
           <p className="text-white sm:text-lg text-sm mt-4 text-left">Nuestra misión en MATRIP es brindarte información detallada y actualizada sobre los lugares más emblemáticos, los secretos locales y los restaurantes que harán que tu experiencia en Madrid sea única.</p>
           <div className="flex flex-col items-center mt-4">
-            <button className="text-slate-100	 bg-zinc-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-4">
+            <button className="text-slate-100	 bg-zinc-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  mt-4">
               <Link href="/dondecomer">¡Quiero comer!</Link>
             </button>
-            <button className="text-slate-100	 bg-zinc-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-4">
+            <button className="text-slate-100	 bg-zinc-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  mt-4">
               <Link href="/quever">¡Quiero visitar un gran lugar!</Link>
             </button>
           </div>
